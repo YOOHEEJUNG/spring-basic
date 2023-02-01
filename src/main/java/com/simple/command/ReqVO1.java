afb75a763df5ee618f6cbe696df6da554b0ab08f
heejung <gmlwjd852369@naver.com>
2023-02-01 19:47:52 +0900

    2023-02-01 mvc2 컨트롤러2023-02-01 mvc2 컨트롤러2023-02-01 mvc2 컨트롤러2023-02-01 mvc2 컨트롤러2023-02-01 mvc2 컨트롤러2023-02-01 mvc2 컨트롤러2023-02-01 mvc2 컨트롤러2023-02-01 mvc2 컨트롤러2023-02-01 mvc2 컨트롤러2023-02-01 mvc2 컨트롤러2023-02-01 mvc2 컨트롤러2023-02-01 mvc2 컨트롤러2023-02-01 mvc2 컨트롤러2023-02-01 mvc2 컨트롤러2023-02-01 mvc2 컨트롤러2023-02-01 mvc2 컨트롤러2023-02-01 mvc2 컨트롤러2023-02-01 mvc2 컨트롤러2023-02-01 mvc2 컨트롤러2023-02-01 mvc2 컨트롤러

---
+++ src/main/java/com/simple/command/ReqVO1.java
@@ -0,0 +1,45 @@
+package com.simple.command;
+
+public class ReqVO1 {
+
+	private String id;
+	private String pw;
+
+	
+	public ReqVO1() {}
+
+
+	public ReqVO1(String id, String pw) {
+		super();
+		this.id = id;
+		this.pw = pw;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public String getPw() {
+		return pw;
+	}
+
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+
+	
+	
+	
+	
+	
+	
+}
---
+++ src/main/webapp/WEB-INF/views/request/ex01.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+	<!-- 화면을 띄우고 싶으면 요청 경로를 만들어줘야 함 -->
+	ex01화면
+
+	<a href="basic1">요청1</a>
+	<a href="basic2">요청2</a>
+	<a href="basic3">요청1</a>
+</body>
+</html>
